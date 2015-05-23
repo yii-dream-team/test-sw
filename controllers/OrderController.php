4ef5d01b58020ef273d821816df080a760f325cc
Alexey Samoylov <alexey.samoylov@gmail.com>
2015-05-23 11:25:46 +0700

    empty order controller

---
+++ controllers/OrderController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Alexey Samoylov <alexey.samoylov@gmail.com>
+ */
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class OrderController extends Controller
+{
+    public function actionAdd()
+    {
+        throw new \BadMethodCallException('Not implemented');
+    }
+
+    public function actionEdit($orderId)
+    {
+        throw new \BadMethodCallException('Not implemented');
+    }
+}
