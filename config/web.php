39537f75c11de8562fc4c2e91a6f7c6e37372ce5
ValentineK <rlng-krsk@yandex.ru>
2015-05-23 11:09:19 +0700

    Url rules + .htaccess file

--- config/web.php
+++ config/web.php
@@ -38,7 +38,10 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
     ],
     'params' => $params,
 ];
