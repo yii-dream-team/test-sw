0263624e97cd10a00d185656a512070f01ae0da0
ValentineK <rlng-krsk@yandex.ru>
2015-05-23 11:07:57 +0700

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=test-sw',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
